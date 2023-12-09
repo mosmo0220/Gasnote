962363f9991629e55d9af6658fcb297871fac882
Marcin Sebastian Osmolak-Rogaluk <mosmo2k@gmail.com>
2023-12-09 22:00:23 +0100

    Apply pylint corrections

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-from typing import Annotated
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
